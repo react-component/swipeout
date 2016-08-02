6fcfa121c4dbb6b1e075305c1d620a3444744466
silentcloud <rjmuqiang@gmail.com>
2016-08-02 10:02:34 +0800

    remove github repos dep

--- src/Swipeout.js
+++ src/Swipeout.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import { View, Text } from 'react-native';
-import Swipe from 'react-native-swipeout';
+import Swipe from 'react-native-swipe-out';
 import splitObject from './util/splitObject';
 
 class Swipeout extends React.Component {
