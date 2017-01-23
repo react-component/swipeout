9c042282afee0c4ae5d9b059619ee3f9bb9a6309
Alberto Schiabel <jkomyno@users.noreply.github.com>
2017-01-23 09:23:19 +0100

    Added Icon capability

--- src/Swipeout.js
+++ src/Swipeout.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { View, Text } from 'react-native';
 import Swipe from 'react-native-swipe-out';
+import Icon as FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import splitObject from './util/splitObject';
 
 class Swipeout extends React.Component {
@@ -35,12 +36,29 @@ class Swipeout extends React.Component {
   renderCustomButton(button) {
     const buttonStyle = button.style || {};
     const bgColor = buttonStyle.backgroundColor || 'transparent';
+    const icon = button.icon || null;
+    const iconStyle = icon && icon.style || {};
+    const iconType = icon && icon.type || 'FontAwesome'; // TODO: support Ionicons, MaterialIcons etc 
+    const iconName = icon && icon.name || null;
+    const iconSize = icon && icon.size || 15;
+    const iconColor = icon && icon.color || 'black';
+    
     const Component = (
       <View
         style={{
           flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: bgColor,
         }}
       >
+      {
+        iconName && (
+          <FontAwesomeIcon
+            name={iconName}
+            color={iconColor}
+            style={iconStyle}
+            size={iconSize}
+          />
+        )
+      }
         <Text style={[button.style, { textAlign: 'center' }]}>
           {button.text}
         </Text>
