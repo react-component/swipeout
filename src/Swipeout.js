4bd62ec6fb246a32e867b0df39522c988a871582
Alberto Schiabel <jkomyno@users.noreply.github.com>
2017-01-23 09:45:49 +0100

    Update Swipeout.js

--- src/Swipeout.js
+++ src/Swipeout.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { View, Text } from 'react-native';
 import Swipe from 'react-native-swipe-out';
-import Icon as FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import splitObject from './util/splitObject';
 
 class Swipeout extends React.Component {
@@ -38,11 +38,12 @@ class Swipeout extends React.Component {
     const bgColor = buttonStyle.backgroundColor || 'transparent';
     const icon = button.icon || null;
     const iconStyle = icon && icon.style || {};
-    const iconType = icon && icon.type || 'FontAwesome'; // TODO: support Ionicons, MaterialIcons etc 
+    // TODO: support Ionicons, MaterialIcons etc
+    // const iconType = icon && icon.type || 'FontAwesome';
     const iconName = icon && icon.name || null;
     const iconSize = icon && icon.size || 15;
     const iconColor = icon && icon.color || 'black';
-    
+
     const Component = (
       <View
         style={{
@@ -51,7 +52,7 @@ class Swipeout extends React.Component {
       >
       {
         iconName && (
-          <FontAwesomeIcon
+          <Icon
             name={iconName}
             color={iconColor}
             style={iconStyle}
