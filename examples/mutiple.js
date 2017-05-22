0990caed290a22c23bfbf148e6316eeb13bcfc28
silentcloud <rjmuqiang@gmail.com>
2017-05-22 16:01:04 +0800

    Updates

--- examples/mutiple.js
+++ examples/mutiple.js
@@ -1,93 +1,83 @@
-webpackJsonp([1],{
+webpackJsonp([2],{
 
-/***/ 0:
-/***/ (function(module, exports, __webpack_require__) {
+/***/ 131:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-	module.exports = __webpack_require__(293);
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__assets_index_less__ = __webpack_require__(44);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__assets_index_less___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__assets_index_less__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__src__ = __webpack_require__(40);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react__ = __webpack_require__(30);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react_dom__ = __webpack_require__(26);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_react_dom__);
+/* eslint-disable no-console */
+/* tslint:disable:no-console */
 
+
+
+
+var SwipeoutExample = __WEBPACK_IMPORTED_MODULE_2_react___default.a.createClass({
+    displayName: 'SwipeoutExample',
+    getInitialState: function getInitialState() {
+        return {
+            items: ['00', '01', '02', '03', '04', '05']
+        };
+    },
+    onDelete: function onDelete(value) {
+        var tempArr = this.state.items;
+        this.setState({
+            items: tempArr.filter(function (v) {
+                return v !== value;
+            })
+        });
+    },
+    render: function render() {
+        var _this = this;
+
+        return __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+            'div',
+            null,
+            __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+                'p',
+                { style: { padding: '16px 30px' } },
+                '\u591A\u4E2A\u5B9E\u4F8B\uFF1A'
+            ),
+            this.state.items.map(function (item, i) {
+                return __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+                    __WEBPACK_IMPORTED_MODULE_1__src__["a" /* default */],
+                    { autoClose: true, key: i, right: [{
+                            text: '\u5220\u9664' + item,
+                            onPress: function onPress() {
+                                return _this.onDelete(item);
+                            },
+                            style: { backgroundColor: '#F4333C', color: 'white' }
+                        }] },
+                    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+                        'div',
+                        { style: { padding: '16px 30px', borderBottom: '1px solid #ccc' }, onClick: function onClick() {
+                                console.log('pressed item ' + item);
+                            } },
+                        'item ',
+                        item
+                    )
+                );
+            })
+        );
+    }
+});
+__WEBPACK_IMPORTED_MODULE_3_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeoutExample, null), document.getElementById('__react-content'));
 
 /***/ }),
 
-/***/ 293:
+/***/ 285:
 /***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	__webpack_require__(2);
-	
-	var _rcSwipeout = __webpack_require__(3);
-	
-	var _rcSwipeout2 = _interopRequireDefault(_rcSwipeout);
-	
-	var _react = __webpack_require__(98);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(135);
-	
-	var _reactDom2 = _interopRequireDefault(_reactDom);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	/* eslint-disable no-console */
-	var SwipeoutExample = _react2.default.createClass({
-	  displayName: 'SwipeoutExample',
-	  getInitialState: function getInitialState() {
-	    return {
-	      items: ['00', '01', '02', '03', '04', '05']
-	    };
-	  },
-	  onDelete: function onDelete(value) {
-	    var tempArr = this.state.items;
-	    this.setState({
-	      items: tempArr.filter(function (v) {
-	        return v !== value;
-	      })
-	    });
-	  },
-	  render: function render() {
-	    var _this = this;
-	
-	    return _react2.default.createElement(
-	      'div',
-	      null,
-	      _react2.default.createElement(
-	        'p',
-	        { style: { padding: '16px 30px' } },
-	        '\u591A\u4E2A\u5B9E\u4F8B\uFF1A'
-	      ),
-	      this.state.items.map(function (item, i) {
-	        return _react2.default.createElement(
-	          _rcSwipeout2.default,
-	          {
-	            autoClose: true,
-	            key: i,
-	            right: [{
-	              text: '\u5220\u9664' + item,
-	              onPress: function onPress() {
-	                return _this.onDelete(item);
-	              },
-	              style: { backgroundColor: '#F4333C', color: 'white' }
-	            }]
-	          },
-	          _react2.default.createElement(
-	            'div',
-	            { style: { padding: '16px 30px', borderBottom: '1px solid #ccc' }, onClick: function onClick() {
-	                console.log('pressed item ' + item);
-	              }
-	            },
-	            'item ',
-	            item
-	          )
-	        );
-	      })
-	    );
-	  }
-	});
-	
-	_reactDom2.default.render(_react2.default.createElement(SwipeoutExample, null), document.getElementById('__react-content'));
+module.exports = __webpack_require__(131);
+
 
 /***/ })
 
-});
+},[285]);
 //# sourceMappingURL=mutiple.js.map
--- examples/simple.js
+++ examples/simple.js
@@ -1,90 +1,78 @@
-webpackJsonp([3],{
+webpackJsonp([0],{
 
-/***/ 0:
-/***/ (function(module, exports, __webpack_require__) {
+/***/ 133:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-	module.exports = __webpack_require__(295);
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__assets_index_less__ = __webpack_require__(44);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__assets_index_less___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__assets_index_less__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__src__ = __webpack_require__(40);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react__ = __webpack_require__(30);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react_dom__ = __webpack_require__(26);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_react_dom__);
+/* eslint-disable no-console */
+/* tslint:disable:no-console */
 
+
+
+
+__WEBPACK_IMPORTED_MODULE_3_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+    'div',
+    { style: { marginBottom: 12 } },
+    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+        __WEBPACK_IMPORTED_MODULE_1__src__["a" /* default */],
+        { style: { backgroundColor: 'white' }, autoClose: true, right: [{ text: 'more',
+                onPress: function onPress() {
+                    return console.log('more');
+                },
+                style: { backgroundColor: 'orange', color: 'white' }
+            }, { text: 'delete',
+                onPress: function onPress() {
+                    return console.log('delete');
+                },
+                style: { backgroundColor: 'red', color: 'white' }
+            }], left: [{
+                text: 'read',
+                onPress: function onPress() {
+                    return console.log('read');
+                },
+                style: { backgroundColor: 'blue', color: 'white' }
+            }, {
+                text: 'reply',
+                onPress: function onPress() {
+                    return console.log('reply');
+                },
+                style: { backgroundColor: 'green', color: 'white' }
+            }], onOpen: function onOpen() {
+                return console.log('open');
+            }, onClose: function onClose() {
+                return console.log('close');
+            } },
+        __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+            'div',
+            { style: {
+                    height: 44,
+                    backgroundColor: 'white',
+                    lineHeight: '44px',
+                    borderTop: '1px solid #dedede',
+                    borderBottom: '1px solid #dedede'
+                } },
+            'swipe out simple demo'
+        )
+    )
+), document.getElementById('__react-content'));
 
 /***/ }),
 
-/***/ 295:
+/***/ 287:
 /***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	__webpack_require__(2);
-	
-	var _rcSwipeout = __webpack_require__(3);
-	
-	var _rcSwipeout2 = _interopRequireDefault(_rcSwipeout);
-	
-	var _react = __webpack_require__(98);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(135);
-	
-	var _reactDom2 = _interopRequireDefault(_reactDom);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	/* eslint-disable no-console */
-	_reactDom2.default.render(_react2.default.createElement(
-	  'div',
-	  { style: { marginBottom: 12 } },
-	  _react2.default.createElement(
-	    _rcSwipeout2.default,
-	    {
-	      style: { backgroundColor: 'white' },
-	      autoClose: true,
-	      right: [{ text: 'more',
-	        onPress: function onPress() {
-	          return console.log('more');
-	        },
-	        style: { backgroundColor: 'orange', color: 'white' }
-	      }, { text: 'delete',
-	        onPress: function onPress() {
-	          return console.log('delete');
-	        },
-	        style: { backgroundColor: 'red', color: 'white' }
-	      }],
-	      left: [{
-	        text: 'read',
-	        onPress: function onPress() {
-	          return console.log('read');
-	        },
-	        style: { backgroundColor: 'blue', color: 'white' }
-	      }, {
-	        text: 'reply',
-	        onPress: function onPress() {
-	          return console.log('reply');
-	        },
-	        style: { backgroundColor: 'green', color: 'white' }
-	      }],
-	      onOpen: function onOpen() {
-	        return console.log('open');
-	      },
-	      onClose: function onClose() {
-	        return console.log('close');
-	      }
-	    },
-	    _react2.default.createElement(
-	      'div',
-	      { style: {
-	          height: 44,
-	          backgroundColor: 'white',
-	          lineHeight: '44px',
-	          borderTop: '1px solid #dedede',
-	          borderBottom: '1px solid #dedede'
-	        }
-	      },
-	      'swipe out simple demo'
-	    )
-	  )
-	), document.getElementById('__react-content'));
+module.exports = __webpack_require__(133);
+
 
 /***/ })
 
-});
+},[287]);
 //# sourceMappingURL=simple.js.map
