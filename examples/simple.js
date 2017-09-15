e3550eee3eb405644d1aaa23049175669d9236d9
silentcloud <rjmuqiang@gmail.com>
2017-09-15 17:03:15 +0800

    Updates

--- examples/simple.js
+++ examples/simple.js
@@ -8,9 +8,9 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_rc_swipeout_assets_index_less__ = __webpack_require__(43);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_rc_swipeout_assets_index_less___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_rc_swipeout_assets_index_less__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_rc_swipeout__ = __webpack_require__(38);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react__ = __webpack_require__(29);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react__ = __webpack_require__(30);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react_dom__ = __webpack_require__(33);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react_dom__ = __webpack_require__(25);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_react_dom__);
 /* eslint-disable no-console */
 /* tslint:disable:no-console */
@@ -71,16 +71,21 @@ __WEBPACK_IMPORTED_MODULE_3_react_dom___default.a.render(__WEBPACK_IMPORTED_MODU
     'div',
     { style: { marginBottom: 12 } },
     __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null),
-    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement('div', { style: { height: 100 } }),
     __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null),
     __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null),
-    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement('div', { style: { height: 100 } }),
+    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null),
+    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null),
+    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null),
+    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null),
+    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null),
+    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null),
+    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null),
     __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(SwipeDemo, null)
 ), document.getElementById('__react-content'));
 
 /***/ }),
 
-/***/ 286:
+/***/ 288:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__(136);
@@ -88,5 +93,5 @@ module.exports = __webpack_require__(136);
 
 /***/ })
 
-},[286]);
+},[288]);
 //# sourceMappingURL=simple.js.map
